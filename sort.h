484e30f07dcfa43e00f4fa3e795f4e31f214ec44
Enigmatik100 <m.p.gangbadja@gmail.com>
2022-12-14 00:47:50 +0000

    Solve compilation errors

--- sort.h
+++ sort.h
@@ -5,6 +5,11 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
 /**
  * struct listint_s - Doubly linked list node
  *
