243bf728441f7e1ca3c0d855bbe9869c8c153015
Enigmatik100 <m.p.gangbadja@gmail.com>
2022-12-14 00:30:25 +0000

    Solve compilation error

--- sort.h
+++ sort.h
@@ -2,6 +2,7 @@
 #define SORT_H
 
 #include <stddef.h>
+#include <stdbool.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -22,7 +23,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-/* advenced tasks */
+/* advanced tasks */
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
